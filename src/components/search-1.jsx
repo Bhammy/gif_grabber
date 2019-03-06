24413b0c9ae57978e38810ab7b1e7bc5f729ad95
Brendan Hamill <brenhamill@gmail.com>
2019-03-05 23:29:12 -0500

    adds additional comments

--- src/components/search-1.jsx
+++ src/components/search-1.jsx
@@ -4,6 +4,13 @@ function Search () {
     // what is a hook?
     // basically, it's a way of letting you "hook into" component lifecycle methods
     // like, most usefully, state - without using classes
+
+    // why would we want to use them?
+    // 1) can reduce the size of components from huge classes into smaller functions
+    // 2) reduce the need to duplicate logic between components
+    // 3) reduce use of complicated patterns like higher order components
+    // 4) minifies better than classes
+    // ...that said, your mileage may vary
     return (
         <div className="search-container">
             Search Giphy here!
